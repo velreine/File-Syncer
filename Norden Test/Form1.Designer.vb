432433aeb331744f674d0a088ea8ab3908e41d59
velreine <nicky-g-hansen@hotmail.com>
2018-06-01 22:21:06 +0200

    Add project files.

---
+++ Norden Test/Form1.Designer.vb
@@ -0,0 +1,199 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.FiveMinuteTMR = New System.Windows.Forms.Timer(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(64, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Path Folder:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(15, 25)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(321, 20)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.Text = "C:\Consultas\Export\Bell\*.gz"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 65)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(95, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Destination Folder:"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(15, 81)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(321, 20)
+        Me.TextBox2.TabIndex = 3
+        Me.TextBox2.Text = "C:\users\dasadmin\Soonr Workplace\Consultas Sync\Bell\otv\"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(384, 25)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(165, 21)
+        Me.ComboBox1.TabIndex = 4
+        Me.ComboBox1.Text = "Guardian"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(381, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(68, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Current Ship:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 404)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(156, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Time since last synchronization:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(174, 404)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(28, 13)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "2:03"
+        '
+        'FiveMinuteTMR
+        '
+        Me.FiveMinuteTMR.Enabled = True
+        Me.FiveMinuteTMR.Interval = 1000
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(90, 252)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 8
+        Me.Button1.Text = "Test"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(90, 216)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 9
+        Me.TextBox3.Text = "300"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(236, 216)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox4.TabIndex = 10
+        Me.TextBox4.Text = "5:00"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(384, 52)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(65, 23)
+        Me.Button2.TabIndex = 11
+        Me.Button2.Text = "Add"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(484, 52)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(65, 23)
+        Me.Button3.TabIndex = 12
+        Me.Button3.Text = "Delete"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(561, 426)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Norden GZ Importer"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents FiveMinuteTMR As System.Windows.Forms.Timer
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+
+End Class
---
+++ Norden Test/Form1.vb
@@ -0,0 +1,218 @@
+﻿Public Class Form1
+    Dim fiveminuteint As Integer = 300
+    Dim shiplist As String = "Gainer,Guardian,Penguin"
+
+
+
+
+
+
+
+
+    Private Sub FiveMinuteTMR_Tick(sender As Object, e As EventArgs) Handles FiveMinuteTMR.Tick
+
+        fiveminuteint -= 1 ' Subtract 1 second from 5 minutes, every time FiveMinuteTMR ticks.
+
+        Label5.Text = ConvertSecondsToMinutes(fiveminuteint)
+
+
+        If (fiveminuteint <= 0) Then ' If it has been five minutes since syncrhonization reset timer.
+
+            fiveminuteint = 300
+
+        End If
+
+
+
+    End Sub
+
+
+
+    Private Function ConvertSecondsToMinutes(ByVal in_seconds As Integer) As String
+        Dim temp_minute As Long = 0
+        Dim temp_seconds As Long = 0
+        ' Divide seconds by 60.
+
+        temp_minute = (in_seconds \ 60)
+
+        temp_seconds = (in_seconds Mod 60)
+
+        If (temp_seconds < 10) Then
+
+            ConvertSecondsToMinutes = (temp_minute.ToString + ":0" + temp_seconds.ToString).ToString
+
+
+        Else
+            ConvertSecondsToMinutes = (temp_minute.ToString + ":" + temp_seconds.ToString).ToString
+
+        End If
+
+
+
+        Return ConvertSecondsToMinutes
+
+
+    End Function
+
+
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+
+
+        TextBox4.Text = ConvertSecondsToMinutes(TextBox3.Text)
+
+
+
+
+
+
+
+    End Sub
+
+    Private Sub TextBox4_TextChanged(sender As Object, e As EventArgs) Handles TextBox4.TextChanged
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim tmp_shiplist() As String
+
+
+
+        tmp_shiplist = Split(shiplist, ",")
+
+        ComboBox1.Text = tmp_shiplist(0) ' Combolist, should contain first entry in shiplist() array.
+
+
+        For index As Integer = 0 To shiplist.Length
+
+            ComboBox1.Items.Add(tmp_shiplist(index))
+
+        Next
+
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim input_fromaddbutton As String = ""
+
+
+        input_fromaddbutton = InputBox("Name of the ship?:", "Vessel Add Button Dialog", "Shipname")
+
+
+
+        If (input_fromaddbutton = Nothing) Then
+            Exit Sub
+        End If
+
+        If (input_fromaddbutton = "Shipname") Then
+
+
+
+            'Do nothing, because that is an invalid input.
+
+        Else
+
+            shiplist = shiplist + "," + input_fromaddbutton
+            ComboBox1.Items.Add(input_fromaddbutton)
+        End If
+
+
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim shipselected As String = ""
+        Dim input_confirmbox As String = ""
+
+        shipselected = ComboBox1.Text
+
+        input_confirmbox = InputBox("Please confirm that you wish to delete [" + shipselected + "] from the list of ships, by typing ""delete""", "Vessel Delete Button Dialog")
+
+
+        If (input_confirmbox = "delete") Then
+
+
+            ' Go for loop to find ship.
+            Dim tmp_shiplist() As String
+            tmp_shiplist = Split(shiplist, ",")
+
+            For index = 0 To tmp_shiplist.Length
+
+                If (tmp_shiplist(index) = shipselected) Then
+
+                    tmp_shiplist(index) = ""
+                    Exit For
+                End If
+
+            Next
+
+            ' Now make another for loop to reconstruct ship list.
+
+            For index = 0 To tmp_shiplist.Length - 1
+
+                If (index = 0) Then
+                    shiplist = "" ' Reset shiplist if first runthrough for loop.
+                End If
+
+                If (tmp_shiplist(index) = "") Then
+
+                    ' Do nothing
+
+
+                Else
+
+                    ' Else if the element at this index is not empty then add it to shiplist.
+
+
+
+                    ' We need this to maintain order in shiplist, so we don't get a comma seperator last time for loop is run through.
+                    If (index = tmp_shiplist.Length - 1) Then
+                        shiplist = shiplist + tmp_shiplist(index)
+                    Else
+                        shiplist = shiplist + tmp_shiplist(index) + ","
+                    End If
+
+
+
+                    End If
+
+
+            Next
+
+
+            ' Need to reset ComboBox1 as well.
+
+            ComboBox1.Items.Clear()
+
+            '<Test>
+            MsgBox(shiplist)
+            '<Test>
+
+            Dim tmp_newlist() As String
+
+            tmp_newlist = Split(shiplist, ",")
+
+            ComboBox1.Items.AddRange(tmp_newlist)
+            ComboBox1.Text = tmp_newlist(0)
+
+
+
+        Else
+
+            ' Do nothing.
+
+
+        End If
+
+
+
+
+    End Sub
+
+   
+
+End Class
